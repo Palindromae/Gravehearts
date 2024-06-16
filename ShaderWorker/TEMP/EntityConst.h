b46d2eea4147c26dcc630a5266ad3f3f696db253
Felix Seanor <astronaut2022@gmail.com>
2024-06-16 12:47:47 +0100

    missed files

--- ShaderWorker/TEMP/EntityConst.h
+++ ShaderWorker/TEMP/EntityConst.h
@@ -18,18 +18,28 @@ const int MaxEntities = 1200;
 #define END_BINDING() 
 #endif
 
-START_BINDING(EntityVec3)
-Position_PresentFrame = 0,
+START_BINDING(PhysicsVec3)
+Position_PastFrame = 0,
 Position_NextFrame = 1,  // Current frame that Physics is calculating
-Velocity_PresentFrame = 2,
+Velocity_PastFrame = 2,
 Velocity_NextFrame = 3,   
 END
 END_BINDING();
 
-START_BINDING(EntityVec4)
+START_BINDING(PhysicsVec4)
 Rotation_PresentFrame = 0,  
 Rotation_NextFrame = 1,    
 AngularVelocity_PresentFrame = 2,   
 AngularVelocity_NextFrame = 3,   
+END
+END_BINDING();
+
+START_BINDING(EntityVec3)
+Position_Interpolated = 0,
+END
+END_BINDING();
+
+START_BINDING(EntityVec4)
+Rotation_Interpolated,
 END
 END_BINDING();
